87630119148e24768f40610656d519f4fe6d2beb
smanoharp <rakeshpanagatla@gmail.com>
2024-11-13 21:23:48 -0600

    valid anagram

---
+++ validAnagram.py
@@ -0,0 +1,16 @@
+def validAnagram(s:str, t:str) -> bool:
+    countS = {}
+    countT = {}
+    if len(s) != len(t):
+        return False
+    
+    for i in range(len(s)):
+        countS[s[i]] = 1 + countS.get(s[i], 0)
+        countT[t[i]] = 1 + countT.get(t[i], 0)
+
+    return countS == countT
+
+testS = "racecar"
+testT = "carrace"
+
+print(validAnagram(testS, testT))
