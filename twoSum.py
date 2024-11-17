189514fdbe8ad2d2fffc2eee2915ebc32f72d677
smanoharp <rakeshpanagatla@gmail.com>
2024-11-16 19:07:37 -0600

    b75 two sum

--- twoSum.py
+++ twoSum.py
@@ -11,7 +11,3 @@ def twoSum(nums:List[int], target) -> List[int]:
 testNums = [2, 7, 11, 15]
 testTarget = 9
 print(twoSum(testNums, testTarget))
-
-testNums1 = [3, 2, 4]
-testTarget1 = 6
-print(twoSum(testNums1, testTarget1))
